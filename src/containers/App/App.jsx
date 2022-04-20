e28b39d422cac1c0ea78054838393e8267d4bcad
Showytam <1the1notorious@gmail.com>
2022-04-20 17:53:45 +0300

    added basename in router

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,12 +1,13 @@
 import routesConfig from '@routes/routesConfig'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from '@components/Header/Header'
+import { REPO_NAME } from '@constants/repo'
 import styles from './App.module.css'
 
 const App = () => {
   return (
     <>
-      <Router >
+      <Router basename={`/${REPO_NAME}/`}>
         <div className={styles.wrapper}>
           <Header />
           <Routes>
